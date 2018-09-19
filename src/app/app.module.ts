defca5cf6e393eade4e94fa9bcdf9ff14436a485
Jonny <jonnytoshen@gmail.com>
2018-09-20 00:11:58 +0800

    feat: test grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgSuiteModule } from 'projects/ng-suite/public_api';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgSuiteModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
